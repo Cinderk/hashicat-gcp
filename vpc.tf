258ac255829aff1b971891b1fd9b96799b081389
Cinderk <cindy.kratochvil@db.com>
2021-11-06 17:11:51 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -4,8 +4,9 @@ module "network" {
   network_name = "gaurav-network"
   project_id = var.project_id
     # insert required variables here
-subnet_name   = "gaurav-subnet"
+subnets = [
+    subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
-
 }
+]
