204db1707ec4bc321ff71eaf3a351cb1184214e5
Cinderk <cindy.kratochvil@db.com>
2021-11-06 15:36:36 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,11 @@
-module "network" {
-  source  = "app.terraform.io/dbacme/network/google"
+module "network_subnets" {
+  source  = "app.terraform.io/dbacme/network/google//modules/subnets"
   version = "3.4.0"
-    
-    project_id   = "var.project"
-    network_name = "gaurav-network"
   # insert required variables here
 }
-subnets = [
+    project_id   = "var.project"
+    network_name = "gaurav-network"
+    subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
